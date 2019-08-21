f3f5d0a11a3581a97b322ad740891caea0ca4256
Mitko <mito_vr@abv.bg>
2019-08-21 09:06:59 +0300

    add street.js

--- Exercise-Components/my-app/src/App.js
+++ Exercise-Components/my-app/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      streets:[],
+      selectedStreetIdx:0,
+      selectedHouseInd:0,
+      hasFetched:false
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+      </div>
+    )
+  }
+  componentWillMount(){
+    fetch("http://localhost:9999/feed/street/all")
+    .then(res=>res.json())
+    .then(data=>{
+      this.setState({
+        streets:data.streets,
+        hasFetched:true
+      })
+    })
+  }
 }
 
 export default App;
